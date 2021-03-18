db33bf7887409c28230a5e089b1226e983ee03ed
Lafferrière <flora.lafferriere@gmail.com>
2021-03-18 18:36:17 +0100

    correction bugs header

--- src/js/main.js
+++ src/js/main.js
@@ -15,19 +15,32 @@
 
 //--------------------Icon search mobile
 
+function removeClass(element, className) {
+    element.classList.remove(className);
+}
+
+function addClass(element, className) {
+    element.classList.add(className);
+}
+
+
 let searchIcon = document.querySelector(".search__icon");
 let searchBar = document.querySelector(".input__search--mobile");
 let mainDark = document.querySelector("main");
+console.log(searchIcon);
+console.log(searchBar);
+console.log(mainDark);
 let cpt = 0;
 
+
 searchIcon.addEventListener("click", event => {
-       cpt++;
+    cpt++;
     if(cpt % 2 == 0) {
-        searchBar.classList.remove("hide");
-        mainDark.classList.add("dark");
+        removeClass(searchBar, 'hide');
+        addClass(mainDark, 'dark')
     } else if(cpt % 2 != 0) {
-        searchBar.classList.add("hide");
-        mainDark.classList.remove("dark");
+        addClass(searchBar, 'hide');
+        removeClass(mainDark, 'dark');
     }
 });
 
@@ -53,69 +66,79 @@ iconMenuOuvert.addEventListener("click", event =>{
 
 //-----------------Menu Desktop
 
-let titresMenuDesktop = document.getElementsByClassName('element__listmenu');
-let soustitreMenuDesktop = document.getElementsByClassName('subtitle');
 // let currentMenu = titresMenuDesktop.nextElementSibling;
 
 // let nouveautes = document.getElementById("text-red");
 // let sousNouveautes = document.getElementById("nouveaute_soustitre");
 
 // nouveautes.addEventListener("mouseover", event => {
-//     sousNouveautes.classList.remove("hide");
-// })
-
-// nouveautes.addEventListener("mouseout", event =>{
-//     sousNouveautes.classList.add("hide");
-// })
-
-//rester sur l'élément affiché quand souris dessus
-
-
-
-
-
-for(let i = 0; i < titresMenuDesktop.length; i++){
-    titresMenuDesktop[i].addEventListener("mouseover", event =>{
-        soustitreMenuDesktop[i].classList.remove("hide");
-        mainDark.classList.add("dark");
-        soustitreMenuDesktop[i].addEventListener("mouseover", event =>{
-            soustitreMenuDesktop[i].classList.remove("hide");
-            mainDark.classList.add("dark");
-            
-        })
-        soustitreMenuDesktop[i].addEventListener("mouseout", event =>{
+    //     sousNouveautes.classList.remove("hide");
+    // })
+    
+    // nouveautes.addEventListener("mouseout", event =>{
+        //     sousNouveautes.classList.add("hide");
+        // })
+        
+        //rester sur l'élément affiché quand souris dessus
+        
+        
+        
+        
+        let titresMenuDesktop = document.getElementsByClassName('element__listmenu');
+        let soustitreMenuDesktop = document.getElementsByClassName('subtitle');
+        
+        for(let i = 0; i < titresMenuDesktop.length; i++){
+            titresMenuDesktop[i].addEventListener("mouseover", event =>{
+                soustitreMenuDesktop[i].classList.remove("hide");
+                mainDark.classList.add("dark");
+                soustitreMenuDesktop[i].addEventListener("mouseover", event =>{
+                    soustitreMenuDesktop[i].classList.remove("hide");
+                    mainDark.classList.add("dark");
+                    
+                })
+                soustitreMenuDesktop[i].addEventListener("mouseout", event =>{
                     soustitreMenuDesktop[i].classList.add("hide");
                     mainDark.classList.remove("dark");
                 })
-    })
-    titresMenuDesktop[i].addEventListener("mouseout", event =>{
-        soustitreMenuDesktop[i].classList.add("hide");
-        mainDark.classList.remove("dark");
-    })
-    // for(i = 0; i < soustitreMenuDesktop.length; i++){
-    //     soustitreMenuDesktop[i].addEventListener("mouseover", event =>{
-    //         soustitreMenuDesktop[i].classList.remove("hide");
-    //     })
-    //     soustitreMenuDesktop[i].addEventListener("mouseout", event =>{
-    //         soustitreMenuDesktop[i].classList.add("hide");
-    //     })
-    // }
-};
-
-
-//___________________FOOTER
+            })
+            titresMenuDesktop[i].addEventListener("mouseout", event =>{
+                soustitreMenuDesktop[i].classList.add("hide");
+                mainDark.classList.remove("dark");
+            })
+            // for(i = 0; i < soustitreMenuDesktop.length; i++){
+                //     soustitreMenuDesktop[i].addEventListener("mouseover", event =>{
+                    //         soustitreMenuDesktop[i].classList.remove("hide");
+                    //     })
+                    //     soustitreMenuDesktop[i].addEventListener("mouseout", event =>{
+                        //         soustitreMenuDesktop[i].classList.add("hide");
+                        //     })
+                        // }
+                    };
+                    
+                    
+                    //___________________FOOTER
+                    
+                    
+let menuIcons = document.querySelector('.menu__icons');
+let iconUser = document.querySelector('.icon__user');
 
 let footerMobileContent = document.querySelector(".footer_mobile_content");
 let footerDesktopContent = document.querySelector(".footer_desktop_content");
-let mediaQuery =  window.matchMedia("(max-width:767px)");
+let mediaQuerySmall =  window.matchMedia("(max-width:767px)");
 
-mediaQuery.addEventListener('change', (e) => {
+mediaQuerySmall.addEventListener('change', (e) => {
     if (e.matches) {
-        footerMobileContent.classList.remove("hide");
-        footerDesktopContent.classList.add("hide");
+        removeClass(menuIcons, 'hide');
+        removeClass(searchIcon, 'hide');
+        removeClass(iconUser, 'hide');
+        removeClass(footerMobileContent, 'hide')
+        addClass(footerDesktopContent, 'hide');
     } else {
-        footerMobileContent.classList.add("hide");
-        footerDesktopContent.classList.remove("hide");
+        addClass(menuIcons, 'hide');
+        addClass(searchIcon, 'hide');
+        addClass(iconUser, 'hide');
 
+        addClass(footerMobileContent, 'hide');
+        removeClass(footerDesktopContent, 'hide');
     }
 })
